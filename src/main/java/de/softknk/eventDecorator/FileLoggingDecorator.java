bc4eedb9f0a3de683675df82f4cd40a8c546403a
csknkdhbw <inf22111@lehre.dhbw-stuttgart.de>
2024-11-03 20:55:37 +0100

    Added specific Decorator classes blueprint

---
+++ src/main/java/de/softknk/eventDecorator/FileLoggingDecorator.java
@@ -0,0 +1,10 @@
+package de.softknk.eventDecorator;
+
+public class FileLoggingDecorator extends EventDecorator {
+
+    @Override
+    public void eventNotify() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
---
+++ src/main/java/de/softknk/eventDecorator/MailEventDecorator.java
@@ -0,0 +1,10 @@
+package de.softknk.eventDecorator;
+
+public class MailEventDecorator extends EventDecorator {
+
+    @Override
+    public void eventNotify() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
